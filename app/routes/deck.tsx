361440904cd92a64902c970189b99a0a77a26807
Jay Son <mooul0529@naver.com>
2025-07-11 16:44:10 +0900

    Feat: Add card info by rank

--- app/routes/deck.tsx
+++ app/routes/deck.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import type { Route } from "./+types/home";
 import Card from "~/components/card";
+import {
+  CARD_POSITION_N,
+  CARD_POSITION_R,
+  CARD_POSITION_SR,
+  CARD_POSITION_SSR,
+} from "~/constants/positions";
+import type { card_info } from "~/constants/interfaces";
 
 const CARD_WIDTH = 112;
 const CARD_HEIGHT = 161;
@@ -17,57 +24,84 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Deck() {
-  const [n, setN] = useState<number | null>(null);
+  const [cardInfo, setCardInfo] = useState<card_info | null>(null);
 
   return (
-    <div className="px-4 py-12">
+    <div className="px-20 py-12">
       <div className="text-center text-6xl">덱</div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-20">
         <div>
           <div className="text-center my-4 text-3xl">SSR</div>
-          <div className="flex flex-wrap justify-center gap-12"></div>
+          <div className="flex flex-wrap justify-center gap-12">
+            {CARD_POSITION_SSR.map(({ x, y }, i) => (
+              <Card
+                key={`${x}-${y}`}
+                width={173}
+                height={241}
+                posX={x}
+                posY={y}
+                setCardInfo={setCardInfo}
+              />
+            ))}
+          </div>
         </div>
         <div>
           <div className="text-center my-4 text-3xl">SR</div>
           <div className="flex flex-wrap justify-center gap-12">
-            {Array(7)
-              .fill(false)
-              .map((_, i) => (
-                <Card
-                  key={i}
-                  width={CARD_WIDTH}
-                  height={CARD_HEIGHT}
-                  posX={-43}
-                  posY={174}
-                  setCardIndex={setN}
-                  index={i}
-                />
-              ))}
-            <div
-              style={{
-                width: `${116}px`,
-                height: `${161}px`,
-                backgroundImage: `url(app/assets/cards.png)`,
-                backgroundPosition: `${-175}px ${174}px`,
-                backgroundSize: `${860}px ${2236}px`,
-              }}
-              className="rounded-lg"
-            />
+            {CARD_POSITION_SR.map(({ x, y }, i) => (
+              <Card
+                key={`${x}-${y}`}
+                width={CARD_WIDTH}
+                height={CARD_HEIGHT}
+                posX={x}
+                posY={y}
+                setCardInfo={setCardInfo}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="text-center my-4 text-3xl">R</div>
+          <div className="flex flex-wrap justify-center gap-12">
+            {CARD_POSITION_R.map(({ x, y }, i) => (
+              <Card
+                key={`${x}-${y}`}
+                width={CARD_WIDTH}
+                height={CARD_HEIGHT}
+                posX={x}
+                posY={y}
+                setCardInfo={setCardInfo}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="text-center my-4 text-3xl">N</div>
+          <div className="flex flex-wrap justify-center gap-12">
+            {CARD_POSITION_N.map(({ x, y }, i) => (
+              <Card
+                key={`${x}-${y}`}
+                width={CARD_WIDTH}
+                height={CARD_HEIGHT}
+                posX={x}
+                posY={y}
+                setCardInfo={setCardInfo}
+              />
+            ))}
           </div>
         </div>
       </div>
-      {n !== null ? (
+      {cardInfo !== null ? (
         <div
           className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center"
-          onClick={() => setN(null)}
+          onClick={() => setCardInfo(null)}
         >
           <div className="absolute top-0 left-0 w-screen h-screen bg-black opacity-50 -z-10"></div>
           <Card
-            width={CARD_WIDTH}
-            height={CARD_HEIGHT}
-            posX={-43}
-            posY={174}
-            index={n}
+            width={cardInfo.width}
+            height={cardInfo.height}
+            posX={cardInfo.x}
+            posY={cardInfo.y}
           />
         </div>
       ) : null}

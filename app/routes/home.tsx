fb0001f1daa3f8247a410a33854ca69c09a15849
sweetandsourkiss <mooul0529@naver.com>
2025-07-14 16:02:44 +0900

    Feat: Update footer

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,22 +6,15 @@ export function meta({}: Route.MetaArgs) {
     { title: "Project CCO" },
     {
       name: "description",
-      content:
-        "침착맨 카드 온라인 홈페이지 입니다. This is the 'Calm Down Man' Card Online(CCO) website.",
+      content: "침착맨 카드 온라인 홈페이지 입니다. This is the 'Calm Down Man' Card Online(CCO) website.",
     },
   ];
 }
 
 export default function Home() {
   return (
-    <div className="sm:h-screen flex flex-col items-center">
-      <img
-        className="mx-auto"
-        src="app/assets/logo.png"
-        alt="침카온 로고 CCO logo"
-        width={500}
-        height={500}
-      />
+    <div className="flex flex-col items-center">
+      <img className="mx-auto" src="app/assets/logo.png" alt="침카온 로고 CCO logo" width={500} height={500} />
       <div className="flex flex-col gap-6">
         <Link
           to={"/bot"}
@@ -35,13 +28,6 @@ export default function Home() {
         >
           덱 구경하기
         </Link>
-      </div>
-      <div className="mt-12 sm:mt-auto p-2 text-xs sm:text-sm">
-        본 웹사이트는 유튜버 침착맨의 팬 활동을 위해 제작된 비상업적
-        사이트입니다. 사이트에 사용된 모든 이미지 및 콘텐츠의 저작권과 초상권은
-        원저작자인 침착맨과 관련 권리자에게 있습니다. 본 사이트는 어떠한 수익도
-        창출하지 않으며, 원저작자의 요청이 있을 시 모든 콘텐츠는 즉시 삭제
-        조치됩니다. [연락처: sweetandsourkiss@gmail.com]
       </div>
     </div>
   );
